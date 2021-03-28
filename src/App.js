3e31847470219a620cf94cda503a19225d00d8a7
Washington <washington.kibichii@radioafricagroup.co.ke>
2021-03-28 09:46:36 +0300

    Api, Set up, card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+    // src/App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    import React, {Component} from 'react';
 
-export default App;
+    class App extends Component {
+      state = {
+        Exchange: []
+      }
+      componentDidMount() {
+        fetch('http://localhost:8080/fetch-local-prices')
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ exchange: data })
+        })
+        .catch(console.log)
+      }
+      render() {
+        return (
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">namee</h5>
+              <h6 className="card-subtitle mb-2 text-muted">jhg</h6>
+              <p className="card-text">dmhasbd</p>
+            </div>
+          </div>
+        );
+      }
+    }
+
+    export default App;
